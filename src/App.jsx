f6b80e77497fde21d2a58be847c1dadee51ad59b
farzamshahid <farzamshahid6@gmail.com>
2024-03-26 15:18:40 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import StarRating from './Components/StarRating'
+
+function App() {
+
+  return (
+    <>
+      <StarRating noOfStars={10} />
+    </>
+  )
+}
+
+export default App
